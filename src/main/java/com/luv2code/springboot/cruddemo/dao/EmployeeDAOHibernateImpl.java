40d0d26b8b5a7815458f82fa1c13e4e2e9f62cb4
Attila Varga <srep.ati@gmail.com>
2021-01-19 15:59:24 +0100

    add dao classes

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
+		
+		List<Employee> employees = theQuery.getResultList();
+		
+		return employees;
+	}
+}
