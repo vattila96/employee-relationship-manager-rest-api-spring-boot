df687ca4d84bb64cdddde64895d70f692ec15f8a
Attila Varga <srep.ati@gmail.com>
2021-01-19 22:43:17 +0100

    change path to "members"

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 }
