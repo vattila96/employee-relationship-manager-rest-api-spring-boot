0c913c32dbabb413ecf1911b6c77cc57471497e8
Attila Varga <srep.ati@gmail.com>
2021-01-21 17:32:18 +0100

    Delete src directory

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.cruddemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.luv2code.springboot.cruddemo.entity.Employee;
-
-//@RepositoryRestResource(path="members")
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	
-}
